bd8341d64751c846ecf4ea5e502b82da2a1225ea
lahirudissanayake15 <lahirudissanayake15@gmail.com>
2022-03-20 18:26:20 +0530

    Add home page

--- index.php
+++ index.php
@@ -1,68 +1,37 @@
-<!-- Not complete -->
-
-<?php
-session_start();
-?>
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <link rel="stylesheet" href="style.css"> -->
-  <title>Home</title>
-  <style>
-    <?php include 'style.css';
-    ?>
-  </style>
+    <title>new_home</title>
+    <link rel="stylesheet" href="new home/home.css">
+
 </head>
 
 <body>
-  <nav>
-    <ul>
-      <?php
-      if (isset($_SESSION["usersId"])) {
-        echo "<li><a href='./profile/profile.php'>Log out</a></li>";
-        echo "<li><a href='./includes/logout.inc.php'>Profile</a></li>";
-      } else {
-        echo "<li><a href='./user/main/login.php'>Login</a></li>";
-        echo "<li><a href='./register/register.php'>Register</a></li>";
-      }
-      ?>
-      <li><a href="./faq/faq.php">Contact</a></li>
-      <li><a href="./support/support.php">Services</a></li>
-      <div id="logo">
-        <a href="index.php">
-          <img src="tecsup-logo.png" width="280px">
-        </a>
-    </ul>
-  </nav>
-  <div class="main">
-    <?php
-    if (isset($_SESSION["userId"])) {
-      echo "<h1>Hello " . $_SESSION["userName"] . "!</h1>";
-    } else {
-      echo "<h1>Hello User!</h1>";
-    }
-    ?>
-    <h2>Welcome to TECSUP</h2></br>
-    <p>Here we provide to customers with a service that solves hardware and software difficulties utilizing an internet platform, saving them time and money.</p>
-    <p>We have the knowledge and experience to assist and resolve issues, and users will receive accurate solutions.</p>
-    <?php
-    if (isset($_SESSION["usersId"])) {
-      echo "<button><a href='./support/supportticket.php'>Support ticket</a></button>";
-    } else {
-      echo "<button><a href='./login/login.php'>Support ticket</a></button>";
-    }
-    ?>
-    <?php
-    if (isset($_SESSION["usersId"])) {
-      echo "<button><a href='./support/supportticket.php'>Support ticket</a></button>";
-    } else {
-      echo "<button><a href='./login/login.php'>Support ticket</a></button>";
-    }
-    ?>
-  </div>
+
+
+    <div class="navbar">
+        <img src="new home/techsup.png" class="logo">
+        <ul>
+            <li><a href="#"> Home </a></li>
+            <li><a href="#"> Register </a></li>
+            <li><a href="user/main/login.php"> Login </a></li>
+            <li><a href="#"> Facilities </a></li>
+            <li><a href="#"> Help </a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h1>Tech support service</h1>
+        <p>we are 24/7 service which can be acessible to anyone anywhere n the country.Here we provide to customers with a service that solves hardware and software difficulties utilizing an internet platform, saving them time and money.
+
+            We have the knowledge and experience to assist and resolve issues, and users will receive accurate solutions. </p>
+        <div>
+            <button type="button"><span></span> Get support!</button>
+        </div>
+    </div>
+
+
+
+
 </body>
 
 </html>
